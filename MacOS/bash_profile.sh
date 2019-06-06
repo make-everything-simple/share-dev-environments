6f1d9c50a64c3e69fbca5ce2aaea18c49bc42d7e
Le Ngoc Duy <askbills@gmail.com>
2019-06-06 11:00:05 +0700

    add useful info android environments
    * show useful android environments
    
    * add utility commands

--- MacOS/bash_profile.sh
+++ MacOS/bash_profile.sh
@@ -44,6 +44,7 @@ alias pidkill="kill -9 $1"
 alias install_sdkman="curl -s https://get.sdkman.io | bash"
 alias install_gpg="brew install gnupg gnupg2"
 alias install_nvm="brew install nvm"
+alias install_gradle="sdk install gradle"
 
 function install_oh_my_zsh() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -78,6 +79,39 @@ function installTools() {
     fi
     echo '==========================================='
 }
+
+#==============================================#
+# install android tools
+#==============================================#
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
+
+export ANDROID_HOME=${HOME}/Library/Android/sdk
+export ANDROID_SDK_ROOT=${HOME}/Library/Android/sdk
+export PATH=${PATH}:${ANDROID_HOME}/tools
+export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+
+function androidTools() {
+  echo '==========================================='
+  echo '1. JDK (Java SE Development Kit). For Java Developers. Includes a complete JRE plus tools for developing, debugging, and monitoring Java applications. 
+        install: https://docs.oracle.com/cd/E19182-01/820-7851/inst_cli_jdk_javahome_t/'
+  echo '2. Android SDK (software development kit) is a set of development tools used to develop applications for Android platform.
+        install: https://developer.android.com/studio'
+  echo '3. IDE for Android: Android Studio, IntelliJ (Optional)'
+  echo '4. Gradle is an open-source build automation tool focused on flexibility and performance. Gradle build scripts are written using a Groovy or Kotlin DSL.
+        install: $ install_gradle'
+  echo 'Noted: check path of JAVA_HOME (ex: jdk1.8.0_51.jdk) in .bash_profile to make sure we use correctly'
+  echo '==========================================='
+}
+
+androidCLIs() {
+  echo 'show list android emulators: $ emulators'
+  echo 'launch a specific emulator :  $startEmulator $emulator_name'
+}
+alias emulators="emulator -list-avds"
+startEmulator() {
+  emulator -avd $1
+}
+
 
 #==============================================#
 # Trigger other scripts run to activate tools
