cd8b5a6eeb3ffe979db36c11f6e5460c40398051
SimpleEverything <lengocduy200186@gmail.com>
2019-06-21 02:11:04 +0700

    refactor(README): update guide and remove file unusable

--- MacOS/bash_active_dev.sh
+++
@@ -1,6 +0,0 @@
-test -r ~/.bash_base && source ~/bash_base
-test -r ~/.bash_pki && source ~/bash_pki
-test -r ~/.bash_android && source ~/bash_android
-test -r ~/.bash_ios && source ~/bash_ios
-test -r ~/.bash_react_native && source ~/bash_react_native
-test -r ~/.bash_end && source ~/bash_end
